adf7f91b1154799022fb97510d78898a42a1a6b3
Sergej-Kotelnikov <110198515+Sergej-Kotelnikov@users.noreply.github.com>
2023-12-06 16:04:53 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,6 +17,13 @@ function myFunction() {
   }
 }
 
+//Preloader
+window.onload = setTimeout(function () {
+  const preloaderEl = document.querySelector(`.mask-loader`);
+  const mainEl = document.querySelector(`main`);     //если вечная загрузка, попробовать заменить на body
+  mainEl.style.display = `flex`;
+  preloaderEl.style.display = `none`;
+}, 4000);
 
 
 
@@ -64,6 +71,82 @@ $(document).ready(function () {
 });
 
 
+
+
+// tabs
+
+function openCity(evt, cityName) {
+  // Объявите все переменные
+  var i, tabcontent, tablinks;
+
+  // Получите все элементы с помощью class="tabcontent" и скройте их.
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Получите все элементы с class="tablinks" и удалите класс "active"."
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Показать текущую вкладку и добавить «активный» класс к кнопке, открывающей вкладку.
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+    
+
+
+
+
+
+
+// lightbox
+
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
 
 
 
